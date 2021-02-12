6c47f66c03bbf2f2142435eca5a36986a539998c
Pob <590650@gmail.com>
2021-02-12 22:31:13 +0700

    init the project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from '@material-ui/core/Button'
+
+function App() {
+  return (
+    <div>
+      <Button color="primary" variant="contained">
+        Hello World
+      </Button>
+    </div>
+  )
+}
+
+export default App
